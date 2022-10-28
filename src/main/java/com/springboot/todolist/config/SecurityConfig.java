1288aa972c95a929c6e4f7d682920f62d5ff5b98
DESKTOP-12AE4RE\dich2 <dich24@naver.com>
2022-10-28 17:51:40 +0900

    스프링 시큐리티와 스웨거 Config 설정

--- src/main/java/com/springboot/todolist/config/SecurityConfig.java
+++ src/main/java/com/springboot/todolist/config/SecurityConfig.java
@@ -1,14 +1,24 @@
 package com.springboot.todolist.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+
+
+    }
 }
--- src/main/java/com/springboot/todolist/config/SwaggerConfig.java
+++ src/main/java/com/springboot/todolist/config/SwaggerConfig.java
@@ -2,31 +2,27 @@ package com.springboot.todolist.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-
-import java.util.HashSet;
-import java.util.Set;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableWebMvc
+@EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
     public Docket restAPI() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .consumes(getConsumeContentTypes())
-                .produces(getProduceContentTypes())
-                .apiInfo(apiInfo()).select()
+                .select()
                 .apis(RequestHandlerSelectors.basePackage("com.springboot.todolist"))
                 .paths(PathSelectors.any())
                 .build()
-                .useDefaultResponseMessages(false);
+                .apiInfo(apiInfo());
+
     }
 
     private ApiInfo apiInfo() {
@@ -35,18 +31,5 @@ public class SwaggerConfig {
                 .version("1.0.0")
                 .description("todoList seongj의 swagger api 입니다.")
                 .build();
-    }
-
-    private Set<String> getConsumeContentTypes() {
-        Set<String> consumes = new HashSet<>();
-        consumes.add("application/json;charset=UTF-8");
-        consumes.add("application/x-www-form-urlencoded");
-        return consumes;
-    }
-
-    private Set<String> getProduceContentTypes() {
-        Set<String> produces = new HashSet<>();
-        produces.add("application/json;charset=UTF-8");
-        return produces;
     }
 }
